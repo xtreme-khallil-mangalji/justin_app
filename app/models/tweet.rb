2fdb755d3f67530e4cb7b6cf012de42909cb52be
xtreme-Khallil-Mangalji <khallil.mangalji@xtremelabs.com>
2012-05-04 13:58:29 -0400

    scheduled

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,26 +1,23 @@
 class Tweet < ActiveRecord::Base
-  attr_accessible :image_url, :text, :title
 
-  def self.refresh
-  	require 'json'
+	attr_accessible :image_url, :text, :title
+
+ 	def self.refresh
+	  	require 'json'
 		require 'net/http'
-		def parse_search
-			url = "http://search.twitter.com/search.json?q=@JustinBieber&rpp=10&include_entities=true&result_type=mixed"
-			resp = Net::HTTP.get_response(URI.parse(url))
-			data = resp.body
+		url = "http://search.twitter.com/search.json?q=@JustinBieber&rpp=10&include_entities=true&result_type=mixed"
+		resp = Net::HTTP.get_response(URI.parse(url))
+		data = resp.body
+		result = JSON.parse(data)
 
-			result = JSON.parse(data)
-
-			if result.has_key? 'Error'
-				raise "twitter error"
-			end
-			return result
+		if result.has_key? 'Error'
+			raise "twitter error"
 		end
 
-		@j_tweets = parse_search["results"]
+		@j_tweets = result["results"]
 
-  		@j_tweets.each do |tweet| 
-			 tweet = Tweet.create(image_url: tweet["profile_image_url"], text: tweet["text"],title: tweet["from_user"])
+	  	@j_tweets.each do |tweet| 
+			tweet = Tweet.create(image_url: tweet["profile_image_url"], text: tweet["text"],title: tweet["from_user"])
 		end
-	end
+ 	end
 end
--- lib/tasks/scheduler.rake
+++ lib/tasks/scheduler.rake
@@ -1,6 +1,7 @@
 desc "This task is called by the Heroku scheduler add-on"
-task :update_feed => :tweets do
+task :update_feed => :environment do
     puts "Updating feed..."
     Tweet.refresh
     puts "done."
 end
+
